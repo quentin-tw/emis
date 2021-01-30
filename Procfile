3392c06797e577630f27e5e9381552c78b255922
Kuen <quentin@DESKTOP-8R0BJPG.localdomain>
2021-01-30 15:01:57 -0800

    running config setup

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn emis_app:app
+web: flask run
--- emis_app/config.py
+++ emis_app/config.py
@@ -9,3 +9,7 @@ class DevConfig:
     SECRET_KEY = environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # FLASK_APP = '__init__.py'
+
+class ProdConfig:
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+    SECRET_KEY = environ.get('SECRET_KEY')
